0274b18d7f78cbb6418585f4f4beddcac637ca9b
karna <112968030+karna-mighty@users.noreply.github.com>
2022-09-26 20:14:05 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,3 +1,18 @@
+import colorama as c
+from colorama import Fore, Back, Style
+
+c.init(autoreset=True)
+
+print(Fore.RED  + Style.BRIGHT  + """
+                                                          _   
+ ___      _   _              ___   _ __   _   _   _ __   | |_ 
+/ __|    | | | |    _____   / __| | '__| | | | | | '_ \  | __|
+\__ \    | |_| |   |_____| | (__  | |    | |_| | | |_) | | |_ 
+|___/uper \__,_|ser         \___| |_|     \__, | | .__/   \__|
+                                          |___/  |_|          
+                                                          """)
+
+
 def encrypt():
     text=input("enter the text to encrypt\n")
     key=input("enter your key to protect \n")
